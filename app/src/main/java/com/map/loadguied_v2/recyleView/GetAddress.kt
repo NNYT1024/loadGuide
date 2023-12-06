b5099702242634f5be193c19f33d4c696a9ca89b
NNYT1024 <gusrl1453@naver.com>
2023-12-06 23:31:43 +0900

    노트북에서 전송

--- app/src/main/java/com/map/loadguied_v2/recyleView/GetAddress.kt
+++ app/src/main/java/com/map/loadguied_v2/recyleView/GetAddress.kt
@@ -3,6 +3,7 @@ package com.map.loadguied_v2.recyleView
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,11 +13,11 @@ class getAddress  : AppCompatActivity() {
 
 
     var AddressList = arrayListOf<AddressItem>(
-        AddressItem("주소 이름","도로명주소"),
-        AddressItem("주소 이름","도로명주소"),
-        AddressItem("주소 이름","도로명주소"),
-        AddressItem("주소 이름","도로명주소"),
-        AddressItem("주소 이름","도로명주소")
+        AddressItem("주소 01","도로명주소"),
+        AddressItem("주소 02","도로명주소"),
+        AddressItem("주소 03","도로명주소"),
+        AddressItem("주소 04","도로명주소"),
+        AddressItem("주소 05","도로명주소")
 
     )
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +26,12 @@ class getAddress  : AppCompatActivity() {
         setContentView(R.layout.select_address_list)
         var Address_RecyclerView = findViewById<RecyclerView>(R.id.Address_RecyclerView)
 
-        val mAdapter = AddressRvAdapter(this, AddressList)
+//        val mAdapter = AddressRvAdapter(this, AddressList)
+
+        val mAdapter = AddressRvAdapter(this, AddressList) { item ->
+            Toast.makeText(this, "주소 이름은 ${item.place_name} 이며, 도로명 주소는 ${item.road_address_name}이다.", Toast.LENGTH_SHORT).show()
+        }
+        Address_RecyclerView.adapter = mAdapter
         Address_RecyclerView.adapter = mAdapter
 
         val lm = LinearLayoutManager(this)
